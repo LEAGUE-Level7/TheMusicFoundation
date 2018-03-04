5ab4c82ec7e13c7c3fce614c9530f92a71f931bf
RationalGeographic <league@WorthyMini-6.attlocal.net>
2018-03-04 14:21:21 -0800

    Music project

---
+++ src/main/java/org/leagueofcole/musicFoundationProject/MusicFoundationProjectApplication.java
@@ -0,0 +1,12 @@
+package org.leagueofcole.musicFoundationProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicFoundationProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicFoundationProjectApplication.class, args);
+	}
+}
